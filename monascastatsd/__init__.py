d123c80f493668941e32c3b53538b12325f9d242
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2016-12-07 07:26:38 +0100

    Make monasca-statsd PEP8 compatible
    Following change ensures that monasca-statsd
    is fully compatible to PEP8.
    
    Change-Id: I15e1be38b62ab029675b8e0aac2784a0ff3f9f56

--- monascastatsd/__init__.py
+++ monascastatsd/__init__.py
@@ -1,6 +1,36 @@
-from monascastatsd.client import Client
-from monascastatsd.connection import Connection
-from monascastatsd.counter import Counter
-from monascastatsd.gauge import Gauge
-from monascastatsd.metricbase import MetricBase
-from monascastatsd.timer import Timer
+# Copyright 2016 FUJITSU LIMITED
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from monascastatsd import client
+from monascastatsd import connection
+from monascastatsd import counter
+from monascastatsd import gauge
+from monascastatsd import metricbase
+from monascastatsd import timer
+
+Client = client.Client
+Connection = connection.Connection
+Counter = counter.Counter
+Gauge = gauge.Gauge
+MetricBase = metricbase.MetricBase
+Timer = timer.Timer
+
+__all__ = [
+    'Client',
+    'Connection',
+    'Counter',
+    'Gauge',
+    'MetricBase',
+    'Timer'
+]
